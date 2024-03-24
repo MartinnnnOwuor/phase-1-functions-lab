ea8ff916d9a537e2d42f3397ce62b4ba2ff82d2f
martin-XPS-15-9510 <oowuoroowuor@gmail.com>
2024-03-24 03:07:40 +0300

    create function.html file

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (thisBLOCK){
+
+    if (thisBLOCK >= 42){
+        return (thisBLOCK - 42);
+    }
+    else (thisBLOCK <= 42);
+        return (42 - thisBLOCK);
+}        
+
+function distanceFromHqInFeet (fEEt) {
+
+    if (fEEt >= 42){
+        return ((fEEt - 42) * 264);
+    }
+    else (fEEt <= 42);
+        return ((42 - fEEt) * 264);
+
+}
+
+function distanceTravelledInFeet (begin, end){
+
+    if (begin >= end){
+        return ((begin - end) * 264);
+    }
+    else (begin <= end);
+        return ((end - begin) * 264);
+
+}
+
+function calculatesFarePrice (start, destination) {
+
+    let feetblock = 264;
+
+    let distance = Math.abs ((destination - start) * 264);
+
+    if (distance <= 400){
+        return 0;
+    }
+    else if (distance <= 2000){ 
+        return ((distance -400) * 0.02);
+    }
+    else if (distance >= 2000 && distance <=2500){
+        return 25;
+
+    }else {
+        return 'cannot travel that far';
+    }
+
+}
